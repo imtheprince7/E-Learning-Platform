d5d8f0a23d46d85575e30967d9727b4f5a87f51d
Prince Kumar <princejnu7@hotmail.com>
2023-01-01 00:43:38 +0530

    new changes

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const { clear } = require('console');
-const express = require('express');
-const path = require('path');
+const { clear } = require("console");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 // setup ejs
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 // path declaration of ejs
-app.set('views', path.join(__dirname, '/views'));
+app.set("views", path.join(__dirname, "/views"));
 //set-up for static serving and using in project
-app.use('/logo', express.static(path.join(__dirname, 'logo')));
+app.use("/", express.static(path.join(__dirname, "static")));
 
-const dire = path.join(__dirname,'/public/views');
-console.log(dire);
 //const conn = require('./database/connection');
-app.get('/', (req ,resp)=>{
-    resp.render('index');
- });
-
-app.get('/public/views/login', (req, resp)=>{
-    resp.render('login')
+app.get("/", (req, resp) => {
+  resp.render("index");
 });
 
-app.get('/public/views/courses.ejs', (req, resp)=>{
-    resp.render('courses')
+app.get('/contact', (req, resp) => {
+    resp.render("contact");
+  });
+
+  app.get('/courses', (req, resp) => {
+  resp.render("courses");
 });
 
+app.get('/feedback', (req, resp) => {
+    resp.render("feedback");
+  });
+
+app.get("/login", (req, resp) => {
+    resp.render("login");
+  });
+
+  app.get("/register", (req, resp) => {
+    resp.render("register");
+  });
 app.listen(4000);
