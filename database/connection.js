c097faaee5185e8521da05b14685e48d6dc1d78c
Prince Kumar <princejnu7@hotmail.com>
2023-01-03 16:24:25 +0530

    new commit

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://0.0.0.0:27017/code-master');
+mongoose.connect('mongodb+srv://admin:admin@cluster0.svzsuz5.mongodb.net/?retryWrites=true&w=majority');
 
  
    const {MongoClient} = require('mongodb');
-   const url ='mongodb://0.0.0.0:27017/code-master';
-   const database = 'code-master';
+   const url ='mongodb+srv://admin:admin@cluster0.svzsuz5.mongodb.net/?retryWrites=true&w=majority';
+   const database = 'e-learning';
    const client = new MongoClient(url);
    
    async function dbConnection()
    {
        let result = await client.connect();
        let db = result.db(database);
-       return db.collection('usersdetails');
+       return db.collection('feedback');
    }
    
    
